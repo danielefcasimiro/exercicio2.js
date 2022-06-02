d2ddcf8c22ebc4b540c4d13d3a8eade89006958b
Daniele Casimiro <104212759+danielefcasimiro@users.noreply.github.com>
2022-06-01 21:02:21 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,8 @@
+//Crie um algoritmo que calcule a velocidade média de um veículo e retorne o resultado no console.
+
+let distanciaPercorrida = 568;
+let tempoGasto = 8;
+let velocidadeMedia = distanciaPercorrida/tempoGasto;
+
+//comando para aparecer no console em uma string interpolation.
+console.log("A velocidade média que fiz o trajeto é de ${velocidadeMedia} km/h.")
